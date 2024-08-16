386e997402677d61cab542c3992eb03068d15486
Abdulkhaleq Dagher <abdodagher24@gmail.com>
2024-08-16 22:54:38 +0300

    create notes view

---
+++ lib/widget/custom_search_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      width: 46,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(0.05),
+      ),
+      child: const Icon(
+        Icons.search,
+        size: 28,
+      ),
+    );
+  }
+}
